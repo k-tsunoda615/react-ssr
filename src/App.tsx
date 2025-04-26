476ab12ae0c1d60169144ef6d02415263b38a8b7
k-tsunoda615 <kntrtnd@gmail.com>
2025-04-26 17:11:24 +0900

    ページの機能を全部落とす

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import Page from './pages/index';
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Page />} />
-    </Routes>
-  );
+  return <Page />;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import { hydrateRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error('Failed to find root element');
-}
+if (!rootElement) throw new Error('Failed to find root element');
 
-// ハイドレーションを使用
 hydrateRoot(
   rootElement,
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
